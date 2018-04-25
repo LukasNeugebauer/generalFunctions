1e1b38c6f745ef4048493ba92fe5e4e3339fb043
LukasNeugebauer <l.neugebauer@uke.de>
2018-04-25 10:35:27 +0200

    minor update, alpha as additional input argument and check's for status of hold

--- plotTransSphere.m
+++ plotTransSphere.m
@@ -1,17 +1,24 @@
 function [] = plotTransSphere(varargin)
-%function [] = plotTransSphere([r,c])
+%function [] = plotTransSphere([r,c,alpha])
 %%Plot a transparant sphere with radius r
+
 
 %handle arguments
 if nargin == 0
-    r = 1;
-    center = [0 0 0];
+    r       = 1;
+    center  = [0 0 0];
 elseif nargin == 1
-    r = varargin{1};
-    center = [0 0 0];
+    r       = varargin{1};
+    center  = [0 0 0];
+    alpha   = 0.2;
 elseif nargin == 2
-    r = varargin{1};
-    center = varargin{2};
+    r       = varargin{1};
+    center  = varargin{2};
+    alpha   = 0.2;
+elseif nargin == 3
+    r       = varargin{1};
+    center  = varargin{2};
+    alpha   = varargin{3};
 end
 
 %prepare data for plotting based on arguments
@@ -20,9 +27,14 @@ x = x.*r + center(1);
 y = y.*r + center(2);
 z = z.*r + center(3);
 
+%make sure to not kill existing plot
+if ~ishold
+    hold on;
+end
+   
 %plotting
 h = surfl(x,y,z);
-set(h,'FaceAlpha',0.2);
+set(h,'FaceAlpha',alpha);
 shading interp;
 
 end
